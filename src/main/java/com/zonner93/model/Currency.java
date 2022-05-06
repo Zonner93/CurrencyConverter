865b7bc664dac42c1a3bda7298a41f97726b58ed
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-05-06 15:58:37 +0200

    add getter and setter in Currency and Rates

--- src/main/java/com/zonner93/model/Currency.java
+++ src/main/java/com/zonner93/model/Currency.java
@@ -1,5 +1,10 @@
 package com.zonner93.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Currency {
     private String table;
     private String currency;
--- src/main/java/com/zonner93/model/Rates.java
+++ src/main/java/com/zonner93/model/Rates.java
@@ -1,5 +1,10 @@
 package com.zonner93.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Rates {
     private String effectiveDate;
     private double bid;
