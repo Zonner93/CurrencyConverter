46bcee5bd796f07644ef74ae671acc10f7e6e617
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-05-06 21:03:59 +0200

    add validation for currency code

---
+++ src/main/java/com/zonner93/constans/AvailableCurrencyCodes.java
@@ -0,0 +1,17 @@
+package com.zonner93.constans;
+
+public enum AvailableCurrencyCodes {
+    usd,
+    aud,
+    cad,
+    eur,
+    huf,
+    chf,
+    gbp,
+    jpy,
+    czk,
+    dkk,
+    nok,
+    sek,
+    xdr
+}
--- src/main/java/com/zonner93/service/web_client/CurrencyServiceImpl.java
+++ src/main/java/com/zonner93/service/web_client/CurrencyServiceImpl.java
@@ -1,16 +1,36 @@
 package com.zonner93.service.web_client;
 
+import com.zonner93.Exception.Currency.CurrencyError;
+import com.zonner93.Exception.Currency.CurrencyException;
+import com.zonner93.constans.AvailableCurrencyCodes;
 import com.zonner93.model.Currency;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CurrencyServiceImpl implements CurrencyService{
+public class CurrencyServiceImpl implements CurrencyService {
     private final WebClientServiceImpl webClientService;
 
     @Override
     public Currency getCurrency(String currencyCode) {
+        validateCurrencyCode(currencyCode);
         return webClientService.getCurrency(currencyCode);
+    }
+
+    private boolean isCurrencyCodeValid(String code) {
+        AvailableCurrencyCodes codes;
+        for (AvailableCurrencyCodes c : AvailableCurrencyCodes.values()) {
+            if (c.name().equals(code)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void validateCurrencyCode(String currencyCode) {
+        if (!isCurrencyCodeValid(currencyCode)) {
+            throw new CurrencyException(CurrencyError.CURRENCY_NOT_FOUND);
+        }
     }
 }
