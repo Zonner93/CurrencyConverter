7c592af47dd7ff4eba5cab1d9c1bff22cb08005d
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-05-06 16:09:11 +0200

    add getCurrency function in WebClientService

--- src/main/java/com/zonner93/service/web_client/WebClientService.java
+++ src/main/java/com/zonner93/service/web_client/WebClientService.java
@@ -1,4 +1,7 @@
 package com.zonner93.service.web_client;
 
+import com.zonner93.model.Currency;
+
 public interface WebClientService {
+    public Currency getCurrency(String currencyCode);
 }
--- src/main/java/com/zonner93/service/web_client/WebClientServiceImpl.java
+++ src/main/java/com/zonner93/service/web_client/WebClientServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zonner93.service.web_client;
 
+import com.zonner93.model.Currency;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,5 +9,10 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class WebClientServiceImpl implements WebClientService{
     private final RestTemplate restTemplate;
+    private final String NBP_API_URL = "https://api.nbp.pl/api/exchangerates/rates/c/";
 
+    @Override
+    public Currency getCurrency(String currencyCode) {
+        return restTemplate.getForObject(NBP_API_URL + currencyCode + "/",Currency.class);
+    }
 }
